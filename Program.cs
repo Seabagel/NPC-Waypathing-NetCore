9e6b46f48b81ac0afc085f361972290b37c5fb7d
Seabagel <hsrt1367tim@gmail.com>
2020-09-07 02:27:59 -0700

    (PSH) dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NPC_Waypathing_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
